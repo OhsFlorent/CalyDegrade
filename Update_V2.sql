df79962f1f305b185b4ecfde9862ff621224f24c
OhsFlorent <florent.petrachi@ohs.asso.fr>
2014-09-25 16:10:20 +0200

    Mise à jour du fichier .db

---
+++ Update_V2.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `base_bainville_mots` (
+	`num_mot`	INTEGER NOT NULL,
+	`ipp_adm`	INTEGER NOT NULL,
+	`nomm`	TEXT NOT NULL,
+	`prenom`	TEXT NOT NULL,
+	`type_mot`	TEXT NOT NULL,
+	`resume_mot`	TEXT NOT NULL,
+	`date`	TEXT NOT NULL,
+	`nom_pre`	TEXT NOT NULL,
+	`text`	INTEGER NOT NULL,
+	PRIMARY KEY(num_mot)
+);
+
+CREATE TABLE `base_flavigny_mots` (
+	`num_mot`	INTEGER NOT NULL,
+	`ipp_adm`	INTEGER NOT NULL,
+	`nomm`	TEXT NOT NULL,
+	`prenom`	TEXT NOT NULL,
+	`type_mot`	TEXT NOT NULL,
+	`resume_mot`	TEXT NOT NULL,
+	`date`	TEXT NOT NULL,
+	`nom_pre`	TEXT NOT NULL,
+	`text`	INTEGER NOT NULL,
+	PRIMARY KEY(num_mot)
+);
